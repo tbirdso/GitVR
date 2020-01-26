6adf7c019f5887fb2fb978932c7f0d9751384c4c
tbirdso <40648863+tbirdso@users.noreply.github.com>
2020-01-25 21:08:50 -0500

    Added Tree Scripts
    Added tree interface and supporting class definitions

---
+++ GitVRUnityProject/Assets/Scripts/TreeNode.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeNode : MonoBehaviour
+{
+    // Hash string representing the commit ID tag
+    public string CommitString;
+    
+    // List representing links to successors, where the length of the list is equivalent to
+    // the number of branches emerging from the node
+    public List<TreeNode> Children;
+
+    // Object representing the parent node. There can only be one true "parent" node.
+    public TreeNode Parent;
+    
+}
