971fe051bc2348852a7c7ea75804ef07c5b2419b
Aaron Mason <apmason13@gmail.com>
2020-01-25 22:19:14 -0500

    Added early Github scripts

---
+++ Assets/Scripts/GitHubTools.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Octokit;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace GitHubTools
+{
+    class GitHubInterface
+    {
+        // variables
+        private GitHubClient client;
+        private ApiInfo apiInfo;
+
+        //constructors
+        public GitHubInterface()
+        {
+            Initialize();
+        }
+
+        public GitHubInterface(string username, string password)
+        {
+            Initialize();
+            client.Credentials = new Credentials(username, password);
+        }
+
+        // Constructor helper functions
+        private void Initialize()
+        {
+            client = new GitHubClient(new ProductHeaderValue("GitVR"));
+        }
+
+        // misc helper functions
+        private void updateApiInfo()
+        {
+            apiInfo = client.GetLastApiInfo();
+        }
+
+        //public void getInfo()
+        //{
+        //    // If the ApiInfo isn't null, there will be a property called RateLimit
+        //    var rateLimit = apiInfo?.RateLimit;
+
+        //    var howManyRequestsCanIMakePerHour = rateLimit?.Limit;
+        //    var howManyRequestsDoIHaveLeft = rateLimit?.Remaining;
+        //    var whenDoesTheLimitReset = rateLimit?.Reset; // UTC time
+
+
+        //    Console.WriteLine(rateLimit);
+        //    Console.WriteLine(howManyRequestsCanIMakePerHour);
+        //    Console.WriteLine(howManyRequestsDoIHaveLeft);
+        //    Console.WriteLine(whenDoesTheLimitReset);
+        //}
+        public int getHourlyRequestLimit()
+        {
+            updateApiInfo();
+            if (apiInfo.RateLimit != null)
+            {
+                return apiInfo.RateLimit.Limit;
+            }
+            return -1;
+        }
+
+        public int getRemainingRequest()
+        {
+            updateApiInfo();
+            if (apiInfo.RateLimit != null)
+            {
+                return apiInfo.RateLimit.Remaining;
+            }
+            return -1;
+        }
+
+        public string getUserInfo(string userid)
+        {
+            var user = client.User.Get(userid);
+            return user.Result.Name + " has " + user.Result.PublicRepos + " public repositories - go check out their profile at " + user.Result.Url;
+        }
+    }
+}
+
---
+++ Assets/Scripts/OctokitTest.cs
@@ -0,0 +1,24 @@
+﻿using GitHubTools;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OctokitTest : MonoBehaviour
+{
+    private GitHubInterface gitHubInterface;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gitHubInterface = new GitHubInterface(TestCredentials.username, TestCredentials.password);
+        print(gitHubInterface.getUserInfo("apmason13"));
+        print("Hourly Request Limit: " + gitHubInterface.getHourlyRequestLimit().ToString());
+        print("Remaining Requests: " + gitHubInterface.getRemainingRequest().ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
