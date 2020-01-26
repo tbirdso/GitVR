5996d0d49ad71eb9226bbf87259e34689c2a0e7f
Aaron Mason <apmason13@gmail.com>
2020-01-26 03:12:26 -0500

    Reorganizing Methods

--- GitVRUnityProject/Assets/Scripts/OctokitTest.cs
+++ GitVRUnityProject/Assets/Scripts/OctokitTest.cs
@@ -16,20 +16,21 @@ public class OctokitTest : MonoBehaviour
 
         if (Debug)
         {
+            //gitHubInterface.UpdateCurrentActiveUser("penis");
             string DebugString = "";
             DebugString += "Hourly Request Limit: " + gitHubInterface.GetHourlyRequestLimit().ToString() + "\n";
             DebugString += "Request Reset Time: " + gitHubInterface.GetRequestLimitResetTime() + "\n";
             DebugString += "\n";
-            DebugString += "Current User's Name: " + gitHubInterface.GetCurrentUsersName() + "\n";
-            DebugString += "Current User's # of Public Repos: " + gitHubInterface.GetCurrentUsersNumberOfPublicRepos() + "\n";
-            DebugString += "Current User's GitHub URL: " + gitHubInterface.GetCurrentUsersGitHubURL() + "\n";
+            DebugString += "Current User's Name: " + gitHubInterface.GetCurrentActiveUsersName() + "\n";
+            DebugString += "Current User's # of Public Repos: " + gitHubInterface.GetCurrentActiveUsersNumberOfPublicRepos() + "\n";
+            DebugString += "Current User's GitHub URL: " + gitHubInterface.GetCurrentActiveUsersGitHubURL() + "\n";
             DebugString += "\n";
             DebugString += "Current User's Public(?) Repositories: " + "\n";
-            foreach (var x in gitHubInterface.GetCurrentUsersRepositories())
+            foreach (var x in gitHubInterface.GetCurrentActiveUsersRepositories())
             {
                 DebugString += x.FullName + "\n";
                 DebugString += "  Branches:" + "\n";
-                foreach (var y in gitHubInterface.getBranchesFromRepo(gitHubInterface.GetCurrentUsersLogin(), x))
+                foreach (var y in gitHubInterface.GetBranchesFromRepo(gitHubInterface.GetCurrentActiveUsersLogin(), x))
                 {
                     DebugString += "    " + y.Name + "\n";
                 }
@@ -38,7 +39,7 @@ public class OctokitTest : MonoBehaviour
                 DebugString += "  Commits:" + "\n";
                 foreach (var z in gitHubInterface.GetCommitsFromRepo(x))
                 {
-                    DebugString += "Message: " + z.Commit.Message + "\n";
+                    DebugString += "Message: " + z.Commit.Message + "\n\n\n";
                 }
                 DebugString += "\n";
             }
