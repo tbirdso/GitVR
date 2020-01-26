e52eee9657a21c67afde0fd5a01f59472b235311
tbirdso <40648863+tbirdso@users.noreply.github.com>
2020-01-25 21:08:50 -0500

    Added Tree Scripts
    Added tree interface and supporting class definitions

---
+++ GitVRUnityProject/Assets/Scripts/GitTree.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitTree : MonoBehaviour, IGitTree
+{
+    public string RepoName { get; set; }
+    public TreeNode HeadNode { get; set; }
+    public List<TreeBranch> Branches { get; set; }
+
+    public TreeBranch getMostRecentBranch()
+    {
+        print("FIXME: Implement branches");
+        return new TreeBranch();
+    }
+
+    public TreeBranch getBranch(string branchName)
+    {
+        print("FIXME: Implement branches");
+        return new TreeBranch();
+    }
+
+    public TreeNode getNthNodeInBranch(string branchName)
+    {
+        print("FIXME: Implement branches");
+        return new TreeNode();
+    }
+}
