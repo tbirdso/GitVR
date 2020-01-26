843ba3d053edbf18949b500b5309b88b53c95c62
Aaron Mason <apmason13@gmail.com>
2020-01-26 00:06:45 -0500

    updated scripts

---
+++ GitVRUnityProject/Assets/Scripts/GitHubTools.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Octokit;
+using System.Threading.Tasks;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace GitHubTools
+{
+    class GitHubInterface
+    {
+        // variables
+        private GitHubClient client;
+        private ApiInfo apiInfo;
+        private User currentUser;
+
+        //constructors
+        public GitHubInterface()
+        {
+            updateCurrentUser();
+        }
+
+        public GitHubInterface(string username, string password)
+        {
+            Initialize();
+            client.Credentials = new Credentials(username, password);
+            updateCurrentUser();
+        }
+
+        public GitHubInterface(string accessToken)
+        {
+            Initialize();
+            client.Credentials = new Credentials(accessToken);
+            updateCurrentUser();
+        }
+
+        // Constructor helper functions
+        private void Initialize()
+        {
+            client = new GitHubClient(new ProductHeaderValue("GitVR"));
+        }
+
+        private void updateCurrentUser()
+        {
+            currentUser = client.User.Current().Result;
+        }
+
+        // misc helper functions
+
+        // updates ApiInfo to the latest available
+        private void updateApiInfo()
+        {
+            apiInfo = client.GetLastApiInfo();
+        }
+
+        // returns the max number of requests per hour
+        public int getHourlyRequestLimit()
+        {
+            updateApiInfo();
+            if (apiInfo.RateLimit != null)
+            {
+                return apiInfo.RateLimit.Limit;
+            }
+            return -1;
+        }
+
+        // returns the number remaining requests
+        public int getRemainingRequest()
+        {
+            updateApiInfo();
+            if (apiInfo.RateLimit != null)
+            {
+                return apiInfo.RateLimit.Remaining;
+            }
+            return -1;
+        }
+
+        // returns the time that the remaining requests will reset back to request limit
+        public DateTimeOffset getRequestLimitResetTime()
+        {
+            updateApiInfo();
+            if (apiInfo.RateLimit != null)
+            {
+                return apiInfo.RateLimit.Reset;
+            }
+            return new DateTimeOffset();
+        }
+
+        // returns the current users First and Last Name
+        public string getCurrentUsersName()
+        {
+            //updateCurrentUser();
+            return currentUser.Name;
+        }
+
+        public int getCurrentUsersNumberOfPublicRepos()
+        {
+            return currentUser.PublicRepos;
+        }
+
+        public string getCurrentUsersGitHubURL()
+        {
+            return currentUser.HtmlUrl;
+        }
+    }
+}
+
---
+++ GitVRUnityProject/Assets/Scripts/OctokitTest.cs
@@ -0,0 +1,30 @@
+﻿using GitHubTools;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OctokitTest : MonoBehaviour
+{
+    private GitHubInterface gitHubInterface;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gitHubInterface = new GitHubInterface(TestCredentials.AccessToken);
+
+        print("Hourly Request Limit: " + gitHubInterface.getHourlyRequestLimit().ToString());
+        print("Remaining Requests: " + gitHubInterface.getRemainingRequest().ToString());
+        print("Request Reset Time: " + gitHubInterface.getRequestLimitResetTime());
+        print("\n");
+        print("Current User's Name: " + gitHubInterface.getCurrentUsersName());
+        print("Current User's # of Public Repos: " + gitHubInterface.getCurrentUsersNumberOfPublicRepos());
+        print("Current User's GitHub URL: " + gitHubInterface.getCurrentUsersGitHubURL());
+        print("\n");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
