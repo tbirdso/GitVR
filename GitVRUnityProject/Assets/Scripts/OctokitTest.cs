cba3de228b253e0306fce51abb64d3e27cbfa5e5
Aaron Mason <apmason13@gmail.com>
2020-01-26 02:29:16 -0500

    Several updates to GitHubTools

--- GitVRUnityProject/Assets/Scripts/OctokitTest.cs
+++ GitVRUnityProject/Assets/Scripts/OctokitTest.cs
@@ -7,19 +7,46 @@ public class OctokitTest : MonoBehaviour
 {
     private GitHubInterface gitHubInterface;
 
+    public bool Debug = false;
+
     // Start is called before the first frame update
     void Start()
     {
         gitHubInterface = new GitHubInterface(TestCredentials.AccessToken);
 
-        print("Hourly Request Limit: " + gitHubInterface.getHourlyRequestLimit().ToString());
-        print("Remaining Requests: " + gitHubInterface.getRemainingRequest().ToString());
-        print("Request Reset Time: " + gitHubInterface.getRequestLimitResetTime());
-        print("\n");
-        print("Current User's Name: " + gitHubInterface.getCurrentUsersName());
-        print("Current User's # of Public Repos: " + gitHubInterface.getCurrentUsersNumberOfPublicRepos());
-        print("Current User's GitHub URL: " + gitHubInterface.getCurrentUsersGitHubURL());
-        print("\n");
+        if (Debug)
+        {
+            string DebugString = "";
+            DebugString += "Hourly Request Limit: " + gitHubInterface.getHourlyRequestLimit().ToString() + "\n";
+            DebugString += "Request Reset Time: " + gitHubInterface.getRequestLimitResetTime() + "\n";
+            DebugString += "\n";
+            DebugString += "Current User's Name: " + gitHubInterface.getCurrentUsersName() + "\n";
+            DebugString += "Current User's # of Public Repos: " + gitHubInterface.getCurrentUsersNumberOfPublicRepos() + "\n";
+            DebugString += "Current User's GitHub URL: " + gitHubInterface.getCurrentUsersGitHubURL() + "\n";
+            DebugString += "\n";
+            DebugString += "Current User's Public(?) Repositories: " + "\n";
+            foreach (var x in gitHubInterface.getCurrentUsersRepositories())
+            {
+                DebugString += x.FullName + "\n";
+                DebugString += "  Branches:" + "\n";
+                foreach (var y in gitHubInterface.getBranchesFromRepo(gitHubInterface.getCurrentUsersLogin(), x))
+                {
+                    DebugString += "    " + y.Name + "\n";
+                }
+                DebugString += "\n";
+
+                DebugString += "  Commits:" + "\n";
+                foreach (var z in gitHubInterface.getCommitsFromRepo(x))
+                {
+                    DebugString += "Message: " + z.Commit.Message + "\n";
+                }
+                DebugString += "\n";
+            }
+
+            DebugString += "Remaining Requests: " + gitHubInterface.getRemainingRequest().ToString();
+
+            print(DebugString);
+        }
     }
 
     // Update is called once per frame
